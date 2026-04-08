482a863c697e834038a380fd4874e21f86738f19
Sudhir Rathod <125038605+sudhirrathod03@users.noreply.github.com>
2026-04-08 20:03:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let map = new Map();
+
+	for(let char of str){
+		map.set(ch,map.get(ch || 0)+1)
+	}
+
+	for(let char of str){
+		if(map.get(ch) === 1) return ch
+	}
+	return -1;
 }
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
